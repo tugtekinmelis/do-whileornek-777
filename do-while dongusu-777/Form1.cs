16e987d9e3ec98069c9099ace39e49077f1ff6fc
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-21 14:00:46 +0300

    Add files via upload

---
+++ do-while dongusu-777/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace do_while_dongusu_777
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            do
+            {
+                MessageBox.Show("döngü calısıyor");
+                if (a == 5)
+                {
+                    MessageBox.Show("döngü durdu");
+                    break;
+                    {
+                        a++;
+                    }
+                }
+            } while (a < 10);
+        }
+    }
+}
